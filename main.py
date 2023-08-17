f093869cb68c6784891852e02d7b9c76cca9b8e4
TheKindDeveloper <129861526+TheKindDeveloper@users.noreply.github.com>
2023-08-17 21:09:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import secrets
 from Dickscord import Style
 from pystyle import *
 import os
-import random
+
+"recoded https://github.com/fknMega/discord-token-changer, to make it unflagged and a few more options."
 
 def _CHECKER_():
     os.system('title Bypass Token Changer ')
@@ -42,14 +43,6 @@ def _CHECKER_():
             response = requests.get("https://discordapp.com/api/v9/experiments", headers=headers)
             self.fingerprint = response.json()["fingerprint"]
 
-        def generate_user_agent(self):
-            user_agents = [
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58",
-            ]
-            return random.choice(user_agents)
-
         def cookies(self):
             if self.cookie is None:
                 Style.print("(!): Couldn't Fetch Cookies.")
@@ -64,24 +57,13 @@ def _CHECKER_():
             cookies_dict = self.cookies()
             return "; ".join([f"{key}={value}" for key, value in cookies_dict.items()])
 
-        def generate_xsuperproperty(self):
-            properties = [
-                "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImRlIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExMi4wLjAuMCBTYWZhcmkvNTM3LjM2IEVkZy8xMTIuMC4xNzIyLjU4IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTEyLjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE5MzkwNiwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ==",
-                "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImRlIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExNS4wLjAuMCBTYWZhcmkvNTM3LjM2IEVkZy8xMTUuMC4xOTAxLjIwMyIsImJyb3dzZXJfdmVyc2lvbiI6IjExNS4wLjAuMCIsIm9zX3ZlcnNpb24iOiIxMCIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjoyMjA0MDQsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"
-            ]
-            return random.choice(properties)
-
     def change_password(email, password, token):
-        Dickcord_extension().request_fingerprint(headers)
-        fingerprint = Dickcord_extension().get_fingerprint()     
-        user_agent = Dickcord_extension().generate_user_agent()   
         s = requests.Session()
-        Dickcord_extension().get_cookies()
-        cookies_header = Dickcord_extension().cookieshead()
-        xsupaprop = Dickcord_extension().generate_xsuperproperty()
-
+        dickcord_extension = Dickcord_extension()
+        dickcord_extension.get_cookies()
+        cookies_header = dickcord_extension.cookieshead()
         headers = {'authorization': token,
-                   'user-agent': user_agent,
+                   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.58',
                    'origin': 'discord.com',
                    'accept': '*/*',
                    'accept-encoding': 'gzip, deflate, br',
@@ -93,8 +75,7 @@ def _CHECKER_():
                    'sec-fetch-mode': 'cors',
                    'sec-fetch-site': 'same-origin',
                    'sec-ch-ua-mobile': '?0',
-                   'x-super-properties': xsupaprop,
-                   'x-fingerprint': fingerprint}
+                   'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImRlIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzExMi4wLjAuMCBTYWZhcmkvNTM3LjM2IEVkZy8xMTIuMC4xNzIyLjU4IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTEyLjAuMC4wIiwib3NfdmVyc2lvbiI6IjEwIiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE5MzkwNiwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ=='}
 
         s.get('https://discord.com/channels/@me', headers=headers)
         if custompass == "n":
@@ -113,6 +94,8 @@ def _CHECKER_():
             return f'1:{email}:{new}:{new_token}'
         if r.status_code == 403:
             Style.print(f"(!): Couldn't Change {email} → {token} is locked")
+        if r.status_code == 401:
+            Style.print(f"(!): Invalid token → {token}")
         else:
             Style.print(f"(!): Failed to change for → ({email})")
             return f'0:{email}:{password}:{token}'
